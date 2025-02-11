f5a18d0925f2a999f8246720aebdb68531c41481
P-PYNSON <phil.pynson@gmail.com>
2025-02-11 08:31:12 +0100

    qsd

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      
       <Stack.Navigator>
         <Stack.Screen name="(homepage)" component={HomepageLayout} options={{ headerShown: false }} />
         {/* <Stack.Screen name="+not-found" /> */}
