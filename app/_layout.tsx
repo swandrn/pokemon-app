a0fded3b1311518dfee1ca6ececfcd23969b5027
P-PYNSON <phil.pynson@gmail.com>
2025-02-11 08:44:30 +0100

    abandoned

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomepageLayout from './(homepage)/_layout';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomepageLayout from "./(homepage)/_layout";
+import NavBar from "@/components/NavBar";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -17,7 +18,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -31,12 +32,12 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack.Navigator>
         <Stack.Screen name="(homepage)" component={HomepageLayout} options={{ headerShown: false }} />
         {/* <Stack.Screen name="+not-found" /> */}
       </Stack.Navigator>
+        <NavBar />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,15 @@
-export default function NavBar({}) {
-    return (
-        <Stac
-    )
+import { Tabs } from "expo-router";
+import { IconSymbol } from "./ui/IconSymbol";
+import { ReactNode, useState } from "react";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { useColorScheme } from "@/hooks/useColorScheme.web";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
 
-} 
+export default function NavBar({  }: { }) {
+  const [hideTabBar, setTabBar] = useState(true);
+  const colorScheme = useColorScheme();
+  return (
+    
+  <View style={'width'}></View>
+  );
+}
