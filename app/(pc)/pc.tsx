1f7abcf82eaa43762ef4fc0fe63e89f18dd55c96
P-PYNSON <phil.pynson@gmail.com>
2025-02-24 08:13:12 +0100

    feature/pc-2.0

--- app/(pc)/pc.tsx
+++ app/(pc)/pc.tsx
@@ -1,40 +1,70 @@
 import { Pokemon } from "@/types/types";
 import { useState } from "react";
-import { Button, Image, Text, TextInput, View } from "react-native";
+import { Button, Image, ScrollView, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {fetchPokemonData} from "../../poke-API/pokemonsDataFetch"
-
+import { fetchPokemonData } from "../../poke-API/pokemonsDataFetch";
+import typeImages from "@/types/images";
 
 export default function PC() {
   const [pokemon, setPokemon] = useState<Pokemon>();
   const [pokemonGameIndex, setPokemonGameIndex] = useState<string>("");
+  const [fetchError, setFetchError] = useState<string>("");
 
   const fetchPokemon = async () => {
+    setFetchError("");
+    if (pokemonGameIndex === "") {
+      return;
+    }
     const pokemon = await fetchPokemonData(pokemonGameIndex);
     setPokemon(pokemon);
-
-  }
+  };
 
   return (
-    <SafeAreaView style={{ flex: 1, flexDirection: "column", padding: 10 }}>
+    <ScrollView style={{ flex: 1, flexDirection: "column", padding: 10 }}>
       <Text style={{ fontSize: 20, fontWeight: "bold", color: "black" }}>PC</Text>
       <View>
         <Text style={{ fontSize: 20, fontWeight: "bold", color: "black" }}>Pokemons</Text>
-        <TextInput placeholder="Pokemon Game index to fetch" value={pokemonGameIndex} onChangeText={(text) => setPokemonGameIndex(text)} />
-        <Button title="Fetch Pokemon" onPress={() => fetchPokemon()} />
+        <TextInput
+          placeholder="Pokemon Game index to fetch"
+          value={pokemonGameIndex}
+          onChangeText={(text) => {
+            const numericText = text.replace(/[^0-9]/g, ""); // Allow only numbers
+            if (parseInt(numericText, 10) <= 898 || numericText === "") {
+              // Ensure the number is not greater than 898
+              setPokemonGameIndex(numericText);
+              setFetchError("");
+            } else {
+              setFetchError("Invalid Pokemon Game index (max is 898)");
+            }
+          }}
+          keyboardType="numeric" // Show numeric keyboard
+        />
+        <Button title="Fetch Pokemon" color={pokemonGameIndex === "" ? "gray" : "blue"} onPress={() => fetchPokemon()} />
+
+        <Text style={{ fontSize: 12, fontWeight: "bold", color: "red" }}>{fetchError ?? ""}</Text>
       </View>
-      <View>
-        <Image source={{ uri: pokemon?.sprites.front }} style={{ width: 100, height: 100 }} />
-        <Text style={{ fontSize: 20, fontWeight: "bold", color: "black" }}>{pokemon?.name}</Text>
+      {pokemon && (
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: "bold", color: "black" }}>{pokemon?.name}</Text>
+          <Image source={{ uri: pokemon?.sprites.front }} style={{ width: 140, height: 140 }} />
+        <View style={{ flexDirection: "row", gap: 10 }}>
+          {pokemon?.types.map((type) => {
+            return <Image key={type.type.name} source={typeImages[type.type.name] || typeImages["default"]} style={{ width: 40, height: 40 }} />;
+          })}
+        </View>
         <Text style={{ fontSize: 20, fontWeight: "bold", color: "black" }}>Stats</Text>
+
         {pokemon?.stats.map((stat) => (
-          <Text key={stat.stat.name}>{stat.stat.name}: {stat.base_stat}</Text>
+          <Text key={stat.stat.name}>
+            {stat.stat.name}: {stat.base_stat}
+          </Text>
         ))}
         <Text style={{ fontSize: 20, fontWeight: "bold", color: "black" }}>Moves</Text>
         {pokemon?.moves.map((move) => (
           <Text key={move.name}>{move.name}</Text>
-        ))}
-      </View>
-    </SafeAreaView>
+          ))}
+        </View>
+      )}
+    </ScrollView>
   );
 }
